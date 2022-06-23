9675dad706042ba53e69530533aef08623f70513
glory <1037380690@qq.com>
2022-06-23 10:06:30 +0800

    config update

--- commit.config.js
+++ commit.config.js
@@ -9,7 +9,7 @@ module.exports = {
         'subject-case': [
             2,
             'never',
-            ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+            ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
         ],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
@@ -29,8 +29,8 @@ module.exports = {
                 'refactor',
                 'revert',
                 'style',
-                'test',
-            ],
-        ],
-    },
+                'test'
+            ]
+        ]
+    }
 }
