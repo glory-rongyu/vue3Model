63ef0aeae90042267ee3a605f2f9e170f2d769f2
glory <1037380690@qq.com>
2022-06-28 17:07:39 +0800

    feat: feat: v-code-diff

--- src/views/demo/vCodeDiff/index.vue
+++ src/views/demo/vCodeDiff/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>v-code-diff</div>
+    <div style="width: 80%">
+        <code-diff :new-string="'3456'" :old-string="'12345'" file-name="test.txt" output-format="side-by-side" />
+    </div>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from 'vue'
-
-onMounted(() => {})
+<script lang="ts" setup>
+import { CodeDiff } from 'v-code-diff'
 </script>
 
 <style scoped></style>
